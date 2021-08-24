e9ad8e5f95faa46b8f73e260a24c8e79e99862ba
KarinAngela <kahnogueira0407@gmail.com>
2021-08-24 19:47:57 +0000

    criação incial dos arquivos

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,11 @@
+package application;
+import org.springframework.boot.SpringeApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class Application{
+    public static void main(String args[]){
+
+        SpringeApplication.run(Application.class, args);
+
+
+    }
+}
---
+++ src/main/java/application/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package application.controllers;
+
+import.org.springframework.stereotype.Controller;
+import.org.springframework.web.bind.annotation.RequestMapping;
+import.org.springframework.web.bind.annotation.Request.Method;
+
+@Controller
+public class HomeController {
+    @RequestMapping(method=RequestiMethod.GET)
+    public String index(){
+        return "home/index.jsp"
+    }
+
+
+
+
+}
